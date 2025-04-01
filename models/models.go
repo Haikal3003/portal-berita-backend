36e039e9baa22e3560c21572c3c4575db94e32f0
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2025-04-01 09:36:42 +0700

    feat: add and refine database models with relationships

--- models/models.go
+++ models/models.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 // RoleType sebagai ENUM
@@ -17,27 +16,111 @@ const (
 
 // Model User
 type User struct {
-	ID       uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Email    string    `json:"email" gorm:"unique;not null"`
-	Password string    `json:"password" gorm:"not null"`
-	Role     RoleType  `json:"role" gorm:"type:varchar(20);not null;default:'USER'"`
+	ID            uuid.UUID      `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	Email         string         `json:"email" gorm:"unique;not null"`
+	Password      string         `json:"password" gorm:"not null"`
+	Role          RoleType       `json:"role" gorm:"type:varchar(20);not null;default:'USER'"`
+	Profile       Profile        `json:"profile" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Articles      []Article      `json:"articles" gorm:"foreignKey:AuthorID"`
+	Notifications []Notification `json:"notifications" gorm:"foreignKey:UserID"`
+	Comments      []Comment      `json:"comments" gorm:"foreignKey:UserID"`
+	Likes         []Like         `json:"likes" gorm:"foreignKey:UserID"`
+	Bookmarks     []Bookmark     `json:"bookmarks" gorm:"foreignKey:UserID"`
 }
 
+// Model Profile
 type Profile struct {
-	ID         uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	UserID     uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
-	Fullname   string    `json:"fullname" gorm:"not null"`
-	Username   string    `json:"username" gorm:"not null"`
-	Bio        string    `json:"bio"`
-	Avatar_URL string    `json:"avatar_url"`
-	Address    string    `json:"address"`
-	Birthdate  string    `json:"birth_date"`
-	Created_At time.Time `json:"created_at" gorm:"autoCreateTime"`
-	Updated_At time.Time `json:"updated_at" gorm:"autoCreateTime"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;unique;not null"`
+	Fullname  string    `json:"fullname" gorm:"not null"`
+	Username  string    `json:"username" gorm:"unique;not null"`
+	Bio       string    `json:"bio"`
+	AvatarURL string    `json:"avatar_url"`
+	Address   string    `json:"address"`
+	Birthdate time.Time `json:"birth_date"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
-// BeforeCreate untuk Generate UUID otomatis
-func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
-	u.ID = uuid.New()
-	return
+// Model Article
+type StatusType string
+
+const (
+	StatusDraft       StatusType = "DRAFT"
+	StatusUnpublished StatusType = "UNPUBLISHED"
+	StatusPublished   StatusType = "PUBLISHED"
+	StatusArchived    StatusType = "ARCHIVED"
+)
+
+type Article struct {
+	ID        uuid.UUID  `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	Title     string     `json:"title" gorm:"not null"`
+	Slug      string     `json:"slug" gorm:"not null;unique"`
+	Content   string     `json:"content" gorm:"not null"`
+	Thumbnail string     `json:"thumbnail"`
+	AuthorID  uuid.UUID  `json:"author_id" gorm:"type:uuid;not null"`
+	Author    User       `json:"author" gorm:"foreignKey:AuthorID"`
+	Status    StatusType `json:"status" gorm:"type:varchar(20);not null;default:'UNPUBLISHED'"`
+	Views     int        `json:"views"`
+	Comments  []Comment  `json:"comments" gorm:"foreignKey:ArticleID"`
+	Likes     []Like     `json:"likes" gorm:"foreignKey:ArticleID"`
+	Bookmarks []Bookmark `json:"bookmarks" gorm:"foreignKey:ArticleID"`
+	CreatedAt time.Time  `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time  `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+// Model Category
+type Category struct {
+	ID        int       `json:"id" gorm:"primary_key;autoIncrement"`
+	Name      string    `json:"name"`
+	Slug      string    `json:"slug" gorm:"not null;unique"`
+	Articles  []Article `json:"articles" gorm:"many2many:article_categories;"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+// Model Tag
+type Tag struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	Name      string    `json:"name"`
+	Slug      string    `json:"slug" gorm:"not null;unique"`
+	Articles  []Article `json:"articles" gorm:"many2many:article_tags;"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+// Model Notification
+type Notification struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
+	Message   string    `json:"message"`
+	IsRead    bool      `json:"isRead"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+// Model Comment
+type Comment struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
+	ArticleID uuid.UUID `json:"article_id" gorm:"type:uuid;not null"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+// Model Like
+type Like struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
+	ArticleID uuid.UUID `json:"article_id" gorm:"type:uuid;not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+}
+
+// Model Bookmark
+type Bookmark struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
+	ArticleID uuid.UUID `json:"article_id" gorm:"type:uuid;not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
