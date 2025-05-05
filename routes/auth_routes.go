6d8ab90a082a03da1ea9b72ca953d70755978c0d
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2025-05-05 16:20:11 +0700

    add air dependency and fix database connection

---
+++ routes/auth_routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"portal-berita-backend/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthRoutes(router fiber.Router, authHandler *handlers.AuthHandler) {
+	auth := router.Group("/auth")
+
+	auth.Post("/register", authHandler.RegisterUser)
+	auth.Post("/login", authHandler.LoginUser)
+}
